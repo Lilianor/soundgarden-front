dd497a2bb19aa726253836249e5c824b0fc47dfd
tiagospeckart <tiagospeckart@gmail.com>
2022-11-18 17:31:08 -0300

    edicao de html para corrigir links

--- js/index.js
+++ js/index.js
@@ -70,7 +70,5 @@ modalForm.addEventListener('submit', event => {
     event_id
   };
 
-  postModal(formData).then(event => {
-    alert(`Você fez ${number_tickets} reserva!`);
-  });
+  postModal(formData);
 });
